97d12ce409019855635dcf6be44a81cec405908d
anreh <anurehka@gmail.com>
2022-08-15 10:05:08 +0800

    Second Commit

--- app/src/main/java/com/example/mycontact/ProfileFragment.kt
+++ app/src/main/java/com/example/mycontact/ProfileFragment.kt
@@ -1,10 +1,19 @@
 package com.example.mycontact
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mycontact.databinding.FragmentSecondBinding
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -16,6 +25,44 @@ class ProfileFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val getProfilePic = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        if(uri != null){
+            binding.imageViewPicture.setImageURI(uri)
+        }
+    }
+
+    private fun saveProfilePicture() {
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+
+        val bd = binding.imageViewPicture.getDrawable() as BitmapDrawable
+        val bitmap = bd.bitmap
+        val outputStream: OutputStream
+
+        try{
+            outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        }catch (e: FileNotFoundException){
+            e.printStackTrace()
+        }
+    }
+
+    private fun readProfilePicture(){
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+
+        try{
+            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+            binding.imageViewPicture.setImageBitmap(bitmap)
+        }catch (e: FileNotFoundException){
+            e.printStackTrace()
+        }
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,14 +84,42 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//
-//        binding.buttonSecond.setOnClickListener {
-//            findNavController().navigate(R.id.action_ProfileFragment_to_ContactFragment)
-//        }
+        setHasOptionsMenu(true)
+        val preferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        if(preferences != null) {
+            if(preferences.contains(getString(R.string.name)))
+                binding.editTextTextName.setText(preferences.getString(getString(R.string.name),""))
+            if(preferences.contains(getString(R.string.phone)))
+                binding.editTextPhone.setText(preferences.getString(getString(R.string.phone),""))
+        }
+
+        binding.imageViewPicture.setOnClickListener {
+                    getProfilePic.launch("image/*")
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when(item.itemId){
+            R.id.action_save -> {
+                val preferences = activity?.getPreferences(Context.MODE_PRIVATE)
+                val name = binding.editTextTextName.text.toString()
+                val phone = binding.editTextPhone.text.toString()
+                if(preferences!=null){
+                    with(preferences.edit()){
+                        putString(getString(R.string.name), name)
+                        putString(getString(R.string.phone), phone)
+                        apply()
+                    }
+                }
+                return true
+            }
+            else -> false
+        }
     }
 }
